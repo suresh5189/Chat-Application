ae96e60f54051c68acf9ef01377dc4483c13d79d
suresh5189 <suru242000@gmail.com>
2023-04-04 23:38:10 +0530

    'Deployed'

--- chat/src/component/chat/Chat.js
+++ chat/src/component/chat/Chat.js
@@ -9,7 +9,7 @@ import closeIcon from "../../images/closeIcon.png";
 
 let socket;
 
-const ENDPOINT = "http://localhost:4500/";
+const ENDPOINT = "https://chat-application-sexq.onrender.com/";
 
 const Chat = () => {
     const [id, setid] = useState("");
